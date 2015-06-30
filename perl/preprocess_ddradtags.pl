ed8e742864657a6ea3e8ef59ec046d672d333af9
har-wradim <jaera@yandex.com>
2015-06-30 14:15:15 +0200

    Code update, no major changes

--- perl/preprocess_ddradtags.pl
+++ perl/preprocess_ddradtags.pl
@@ -111,7 +111,7 @@ if (defined($args{-y})) {
 	delete($args{-y});
 }
 else {
-	print STDERR "Number of mismatches in barcodes not specified: taking the defaulft of $y\n";
+	print STDERR "Number of mismatches in barcodes not specified: taking the default of $y\n";
 }
 if (defined($args{-t})) {
 	$t = $args{-t};
@@ -137,12 +137,12 @@ else {
 
 if (defined($args{-lrest})) {
 	$lrest = $args{-lrest};
-	die("Invalid restriction site '$lrest'\n") if ($lrest !~ /^[ATGC]+$/);
+	die("Invalid restriction site '$lrest'\n") if $lrest !~ /^[ATGC]+$/;
 	delete($args{-lrest});
 }
 if (defined($args{-rrest})) {
 	$rrest = $args{-rrest};
-	die("Invalid restriction site '$rrest'\n") if ($rrest !~ /^[ATGC]+$/);
+	die("Invalid restriction site '$rrest'\n") if $rrest !~ /^[ATGC]+$/;
 	delete($args{-rrest});
 }
 if (defined($args{-gz})) {
@@ -178,189 +178,6 @@ my %loci;
 my ($locus, $mask);
 my @i_rescue;
 
-## BEGIN SUBS ##
-
-## read the barcodes from the files ##
-sub read_barcodes {
-	open(BARCODES, "<", $bfile) or die("Couldn't open barcode file\n");
-	my (@all_barcodes, $len, @parts);
-	my (%no_dbrs_hash, %i_shifts_hash);
-	while (my $line = <BARCODES>) {
-		chomp $line;
-		if ($line =~ /^\d+$/) {
- 			$bl = $line;
-			next;
-		}
-		die("The barcode file has incorrect format. At least three fields expected: the barcode, the presence-of-DBR-flag, P7 offset\n") if ($line !~ /^[ATGCX]+\s+(1|0)\s+\d(\s+\w+)?$/);
-		@parts = split ' ', $line;
-		$b = $parts[0];
-		push(@all_barcodes, $b);
-		$no_dbrs_hash{$b} = !$parts[1];
-		$i_shifts_hash{$b} = $parts[2];
-		$len = length($b);
-		if (!defined($bl) or $bl > $len) {
-			$bl = $len;
-		}
-		if (defined($parts[3])) {
-			$barcode_names{$b} = $parts[3];
-		}
-	}
-	close(BARCODES);
-	my $subb;
-	foreach my $b (@all_barcodes) {
-		$subb = substr($b, 0, $bl);
-		if (defined($barcode_names{$b}) && $subb ne $b) {
-			$barcode_names{$subb} = $barcode_names{$b};
-			delete($barcode_names{$b});
-		}
-		push(@barcodes, $subb);
-		$barcode_lens[$#barcodes] = length($b);
-		$no_dbrs[$#barcodes] = $no_dbrs_hash{$b};
-		$i_shifts[$#barcodes] = $i_shifts_hash{$b};
-		$skipped{$subb} = 0;
-		$uniq{$subb} = ();
-	}
-}
-
-## make a regex pattern out of DBR ##
-sub prepare_dbr {
-	my $pattern = shift;
-	$pattern =~ s/R/[AG]/g;
-	$pattern =~ s/Y/[CT]/g;
-	$pattern =~ s/S/[GC]/g;
-	$pattern =~ s/W/[AT]/g;
-	$pattern =~ s/K/[GT]/g;
-	$pattern =~ s/M/[AC]/g;
-	$pattern =~ s/B/[CGT]/g;
-	$pattern =~ s/D/[AGT]/g;
-	$pattern =~ s/H/[ACT]/g;
-	$pattern =~ s/V/[ACG]/g;
-	$pattern =~ s/N/[ACGTNX]/g;
-	return $pattern;
-}
-
-## get DBR and create pattern out of it ##
-sub read_dbr {
-	$il = length($orig_dbr);
-	$dbr = prepare_dbr($orig_dbr);
-	my $var_dbr = $orig_dbr;
-	$var_dbr =~ s/[ATGC]+$//g;
-	$icut = length($var_dbr) - length($orig_dbr);
-	my @i_bases = split //, $orig_dbr;
-	for my $pos (0..$#i_bases) {
-		my $i_base = $i_bases[$pos];
-		next if $i_base eq 'N';
-		$i_base =~ tr/ACGTRYMKBDHVYRKM/BDHVYRKMACGTRYMK/;
-		push(@i_rescue, prepare_dbr(substr($orig_dbr, 0, $pos).$i_base.substr($orig_dbr, $pos + 1)) );
-	}
-}
-
-## identify barcode in a read ##
-sub find_similar_barcode {
-	my $seq = shift;
-	my $b;
-	$b_chosen = "";
-	$i_shift = -1;
-	foreach my $j (0..$t) {
-		$subseq = substr($seq, $j, $bl);
-		foreach my $k (0..$#barcodes) {
-			$b = $barcodes[$k];
-			$mask = $b ^ $subseq;
-			$mask =~ tr/\0//d;
-			if (length($mask) <= $y) {
-				$b_chosen = $b;
-				$no_dbr = $no_dbrs[$k];
-				$i_shift = $i_shifts[$k];
-				return $barcode_lens[$k] + $j;
-			}
-		}
-	}
-	return -1;
-}
-
-## identify dbr ##
-sub find_dbr {
-	my $seq = shift;
-	$subseq = substr($seq, $i_shift, $il);
-	foreach my $j (0..$t) {
-		my $subseqj = $j ? substr($seq, $i_shift + $j, $il) : $subseq;
-		if ($subseqj =~ /^($dbr)/) {
-			$i_chosen = $icut < 0 ? substr($1, 0, $icut) : $1;
-			$dbrs{$i_chosen}++;
-			return $il + $i_shift + $j;
-		}
-	}
-	foreach my $rescue (@i_rescue) {
-		if ($subseq =~ /^($rescue)/) {
-			$i_chosen = $icut < 0 ? substr($1, 0, $icut) : $1;
-			$dbrs{$i_chosen}++;
-			return $il + $i_shift;
-		}
-	}
-	return -1;
-}
-
-## detect if this is a duplicate ##
-sub is_duplicate {
-	$seq = shift;
-	$subseq1 = substr($seq, 0,  $m);
-	$subseq2 = substr($seq, $m, $m);
-	$locus = $pieces12{$b_chosen}{$subseq1}{$subseq2};
-	if (!defined($locus)) {
-		$subseq3 = substr($seq, $m2, $m);
-		$locus = $pieces13{$b_chosen}{$subseq1}{$subseq3};
-		if (!defined($locus)) {
-			$locus = $pieces23{$b_chosen}{$subseq2}{$subseq3};
-			if (!defined($locus)) {
-				$locus = push(@{$loci{$b_chosen}}, $subseq1.$subseq2.$subseq3) - 1;
-				$pieces23{$b_chosen}{$subseq2}{$subseq3} = $locus;
-			}
-			$pieces13{$b_chosen}{$subseq1}{$subseq3} = $locus;
-		}
-		$pieces12{$b_chosen}{$subseq1}{$subseq2} = $locus;
-	}
-	if ($no_dbr) {
-		return (++$uniq{$b_chosen}[$locus]{"no_dbr"} > 1);
-	}
-	if ($x) {
-		foreach my $dbr (keys %{$uniq{$b_chosen}[$locus]}) {
-			$mask = $i_chosen ^ $dbr;
-			$mask =~ tr/\0//d;
-			if (length($mask) <= $x) {
-				++$uniq{$b_chosen}[$locus]{$dbr};
-				return 1;
-			}
-		}
-	}
-	return (++$uniq{$b_chosen}[$locus]{$i_chosen} > 1);
-}
-
-sub openin {
-	my $fname = shift;
-	my $fp;
-	if ($fname =~ /\.gz$/) {
-		open($fp, "gzip -cd $fname |") or die($!);
-	}
-	else {
-		open($fp, "<",  $fname) or die($!);
-	}
-	return $fp;
-}
-
-sub openout {
-	my $fname = shift;
-	my $gz = shift;
-	my $fp;
-	if ($gz eq 'y') {
-		open($fp, "| gzip -c > $fname.gz") or die($!);
-	}
-	else {
-		open($fp, ">", $fname) or die($!);
-	}
-	return $fp;
-}
-
-## END SUBS ##
 
 read_barcodes();
 read_dbr();
@@ -376,35 +193,34 @@ foreach my $k (0..$#barcodes) {
 	$b = $barcodes[$k];
 	my $lfix = defined($barcode_names{$b}) ? $barcode_names{$b}.".l" : "$lfile.$b";
 	my $rfix = defined($barcode_names{$b}) ? $barcode_names{$b}.".r" : "$rfile.$b";
-	$LABB{$b} = openout("$lfix.abb", $gz);
-	$RABB{$b} = openout("$rfix.abb", $gz);
+	$LABB{$b} = openout("$lfix.abb");
+	$RABB{$b} = openout("$rfix.abb");
 	
 	if ($no_dbrs[$k]) {
-		$LNOI{$b} = openout("$lfix.noi", $gz);
-		$RNOI{$b} = openout("$rfix.noi", $gz);
+		$LNOI{$b} = openout("$lfix.noi");
+		$RNOI{$b} = openout("$rfix.noi");
 	}
 	else {
-		$LWBI{$b} = openout("$lfix.wbi", $gz);
-		$RWBI{$b} = openout("$rfix.wbi", $gz);
-		$LWOI{$b} = openout("$lfix.woi", $gz);
-		$RWOI{$b} = openout("$rfix.woi", $gz);
+		$LWBI{$b} = openout("$lfix.wbi");
+		$RWBI{$b} = openout("$rfix.wbi");
+		$LWOI{$b} = openout("$lfix.woi");
+		$RWOI{$b} = openout("$rfix.woi");
 	}
 	if ($rrest) {
-		$LRUR{$b} = openout("$rfix.rur", $gz);
-		$RRUR{$b} = openout("$lfix.rur", $gz);
+		$LRUR{$b} = openout("$rfix.rur");
+		$RRUR{$b} = openout("$lfix.rur");
 	}
 	if ($lrest) {
-		$LLUR{$b} = openout("$rfix.lur", $gz);
-		$RLUR{$b} = openout("$lfix.lur", $gz);
+		$LLUR{$b} = openout("$rfix.lur");
+		$RLUR{$b} = openout("$lfix.lur");
 	}
 }
 
-$LWOB = openout("$lfile.wob", $gz);
-$RWOB = openout("$rfile.wob", $gz);
+$LWOB = openout("$lfile.wob");
+$RWOB = openout("$rfile.wob");
 
 ## END FILES ##
 
-my $mod;
 my $eof;
 
 $lline = <$lin>;
@@ -431,106 +247,79 @@ my $total = 0;
 
 my $time;
 while (!$eof) {
-	$lline = <$lin>;
-	$rline = <$rin>;
-	chomp $lline;
-	chomp $rline;
-	$mod = $. % 4;
+	report_time() if ++$total % 1000000 == 0;
+	push(@lfq, scalar <$lin>) foreach (0..3);
+	push(@rfq, scalar <$rin>) foreach (0..3);
 	$eof = (eof($lin) or eof($rin) or !$lline or !$rline);
-	## if it is the quality line ##
-	if ($mod == 0) {
-		$lq = $lline;
-		$rq = $rline;
-		if ($bl_chosen >= 0) {
-			$lq = substr($lq, $bl_chosen, $lt);
-			if ($no_dbr) {
-				$rq = substr($rq, $i_shift, $rt);
-			}
-			elsif ($i_offset >= 0) {
-				$rq = substr($rq, $i_offset, $rt);
-			}
-		}
-	}
-	## if it is header or end of file ##
-	elsif ($mod == 1 || $eof) {
-		if ($is_dup) {
-			$skipped{$b_chosen}++;
-			$dupc++;
-		}
-		else {
-			$l = sprintf("@%s\n%s\n+\n%s\n", $lh, $ls, $lq);
-			$r = sprintf("@%s\n%s\n+\n%s\n", $rh, $rs, $rq);
-			if ($bl_chosen < 0) {
-				print { $LWOB } $l;
-				print { $RWOB } $r;
-				$wobc++;
-			}
-			elsif ($no_dbr) {
-				print { $LNOI{$b_chosen} } $l;
-				print { $RNOI{$b_chosen} } $r;
-				$noic++;
-			}
-			elsif ($i_offset < 0) {
-				print { $LWOI{$b_chosen} } $l;
-				print { $RWOI{$b_chosen} } $r;
-				$woic++;
-			}
-			elsif ($lrest && index($l, $lrest) > -1) {
-				print { $LLUR{$b_chosen} } $l;
-				print { $RLUR{$b_chosen} } $r;
-				$lurc++;
-			}
-			elsif ($rrest && index($r, $rrest) > -1) {
-				print { $LRUR{$b_chosen} } $l;
-				print { $RRUR{$b_chosen} } $r;
-				$rurc++;
-			}
-			elsif (length($ls) < $lt || length($rs) < $rt) {
-				print { $LABB{$b_chosen} } $l;
-				print { $RABB{$b_chosen} } $r;
-				$abbc++;
-			}
-			else {
-				print { $LWBI{$b_chosen} } $l;
-				print { $RWBI{$b_chosen} } $r;
-				$wbic++;
-			}
-		}
-		last if $eof;
-		$lh = substr($lline, 1);
-		$rh = substr($rline, 1);
-	}
-	## if it is the "+"-line ##
-	elsif ($mod == 3) {
-		die("Files are not in phase: $lline\n") if ($lline ne "+" or $rline ne "+");
-		if ($total % 1000000 == 0) {
-			$time = time() - $start_time;
-			printf STDERR "Read pair #%d, %d sec, %d reads/sec\n", $total, $time, $total / $time if $time > 0;
-		}
-		$total++;
-	}
-	## if it is the sequence line ##
-	elsif ($mod == 2) {
-		$ls = $lline;
-		$rs = $rline;
-		$is_dup = 0;
-		$bl_chosen = find_similar_barcode($ls);
-		next if $bl_chosen < 0;
-
+	$lh = <$lin>;
+	$rh = <$rin>;
+	$ls = <$lin>;
+	$rs = <$rin>;
+	die("The files are out of phase at line $.\n") if <$lin> ne "+" || <$rin> ne "+";
+	$lq = <$lin>;
+	$rq = <$rin>;
+	chomp($ls, $rs, $lh, $rh, $lq, $rq);
+	$is_dup = 0;
+	$bl_chosen = find_similar_barcode($ls);
+	if ($bl_chosen >= 0) {
 		$ls = substr($ls, $bl_chosen, $lt);
-
+		$lq = substr($lq, $bl_chosen, $lt);
 		if ($no_dbr) {
 			$rs = substr($rs, $i_shift, $rt);
+			$rq = substr($rq, $i_shift, $rt);
 			is_duplicate($rs);
-			next;
 		}
-
-		$i_offset = find_dbr($rs);
-		next if $i_offset < 0;
-
-		$rs = substr($rs, $i_offset, $rt);
-		next if ($is_dup = is_duplicate($rs));
-		$rh .= " ".$i_chosen;
+		else {
+			$i_offset = find_dbr($rs);
+			if ($i_offset >= 0) {
+				$rs = substr($rs, $i_offset, $rt);
+				$rq = substr($rq, $i_offset, $rt);
+				$rh .= " ".$i_chosen if !($is_dup = is_duplicate($rs));
+			}
+		}
+	}
+	if ($is_dup) {
+		$skipped{$b_chosen}++;
+		$dupc++;
+	}
+	else {
+		$l = sprintf("%s\n%s\n+\n%s\n", $lh, $ls, $lq);
+		$r = sprintf("%s\n%s\n+\n%s\n", $rh, $rs, $rq);
+		if ($bl_chosen < 0) {
+			print { $LWOB } @lfq;
+			print { $RWOB } @rfq;
+			$wobc++;
+		}
+		elsif ($no_dbr) {
+			print { $LNOI{$b_chosen} } @lfq;
+			print { $RNOI{$b_chosen} } @rfq;
+			$noic++;
+		}
+		elsif ($i_offset < 0) {
+			print { $LWOI{$b_chosen} } @lfq;
+			print { $RWOI{$b_chosen} } @rfq;
+			$woic++;
+		}
+		elsif ($lrest && index($ls, $lrest) > -1) {
+			print { $LLUR{$b_chosen} } @lfq;
+			print { $RLUR{$b_chosen} } @rfq;
+			$lurc++;
+		}
+		elsif ($rrest && index($rs, $rrest) > -1) {
+			print { $LRUR{$b_chosen} } @lfq;
+			print { $RRUR{$b_chosen} } @rfq;
+			$rurc++;
+		}
+		elsif (length($ls) < $lt || length($rs) < $rt) {
+			print { $LABB{$b_chosen} } @lfq;
+			print { $RABB{$b_chosen} } @rfq;
+			$abbc++;
+		}
+		else {
+			print { $LWBI{$b_chosen} } @lfq;
+			print { $RWBI{$b_chosen} } @rfq;
+			$wbic++;
+		}
 	}
 }
 
@@ -633,3 +422,180 @@ while (my ($i, $count) = each (%dbrs)) {
 }
 print STDERR "\nDone\n";
 close(INT);
+
+
+## read the barcodes from the files ##
+sub read_barcodes {
+	open(BARCODES, "<", $bfile) or die("Couldn't open barcode file\n");
+	my (@all_barcodes, $len, @parts);
+	my (%no_dbrs_hash, %i_shifts_hash);
+	while (my $line = <BARCODES>) {
+		chomp $line;
+		if ($line =~ /^\d+$/) {
+ 			$bl = $line;
+			next;
+		}
+		die("The barcode file has incorrect format. At least three fields expected: the barcode, the presence-of-DBR-flag, P7 offset\n") if ($line !~ /^[ATGCX]+\s+(1|0)\s+\d(\s+\w+)?$/);
+		@parts = split ' ', $line;
+		$b = $parts[0];
+		push(@all_barcodes, $b);
+		$no_dbrs_hash{$b} = !$parts[1];
+		$i_shifts_hash{$b} = $parts[2];
+		$len = length($b);
+		if (!defined($bl) or $bl > $len) {
+			$bl = $len;
+		}
+		if (defined($parts[3])) {
+			$barcode_names{$b} = $parts[3];
+		}
+	}
+	close(BARCODES);
+	my $subb;
+	foreach my $b (@all_barcodes) {
+		$subb = substr($b, 0, $bl);
+		if (defined($barcode_names{$b}) && $subb ne $b) {
+			$barcode_names{$subb} = $barcode_names{$b};
+			delete($barcode_names{$b});
+		}
+		push(@barcodes, $subb);
+		$barcode_lens[$#barcodes] = length($b);
+		$no_dbrs[$#barcodes] = $no_dbrs_hash{$b};
+		$i_shifts[$#barcodes] = $i_shifts_hash{$b};
+		$skipped{$subb} = 0;
+		$uniq{$subb} = ();
+	}
+}
+
+## make a regex pattern out of DBR ##
+sub prepare_dbr {
+	my $pattern = shift;
+	$pattern =~ s/R/[AG]/g;
+	$pattern =~ s/Y/[CT]/g;
+	$pattern =~ s/S/[GC]/g;
+	$pattern =~ s/W/[AT]/g;
+	$pattern =~ s/K/[GT]/g;
+	$pattern =~ s/M/[AC]/g;
+	$pattern =~ s/B/[CGT]/g;
+	$pattern =~ s/D/[AGT]/g;
+	$pattern =~ s/H/[ACT]/g;
+	$pattern =~ s/V/[ACG]/g;
+	$pattern =~ s/N/[ACGTNX]/g;
+	return $pattern;
+}
+
+## get DBR and create pattern out of it ##
+sub read_dbr {
+	$il = length($orig_dbr);
+	$dbr = prepare_dbr($orig_dbr);
+	my $var_dbr = $orig_dbr;
+	$var_dbr =~ s/[ATGC]+$//g;
+	$icut = length($var_dbr) - length($orig_dbr);
+	my @i_bases = split //, $orig_dbr;
+	for my $pos (0..$#i_bases) {
+		my $i_base = $i_bases[$pos];
+		next if $i_base eq 'N';
+		$i_base =~ tr/ACGTRYMKBDHVYR/BDHVYRKMACGTRY/;
+		push(@i_rescue, prepare_dbr(substr($orig_dbr, 0, $pos).$i_base.substr($orig_dbr, $pos + 1)) );
+	}
+}
+
+## identify barcode in a read ##
+sub find_similar_barcode {
+	my $seq = shift;
+	my $b;
+	$b_chosen = "";
+	$i_shift = -1;
+	foreach my $j (0..$t) {
+		$subseq = substr($seq, $j, $bl);
+		foreach my $k (0..$#barcodes) {
+			$b = $barcodes[$k];
+			$mask = $b ^ $subseq;
+			$mask =~ tr/\0//d;
+			if (length($mask) <= $y) {
+				$b_chosen = $b;
+				$no_dbr = $no_dbrs[$k];
+				$i_shift = $i_shifts[$k];
+				return $barcode_lens[$k] + $j;
+			}
+		}
+	}
+	return -1;
+}
+
+## identify dbr ##
+sub find_dbr {
+	my $seq = shift;
+	$subseq = substr($seq, $i_shift, $il);
+	foreach my $j (0..$t) {
+		my $subseqj = $j ? substr($seq, $i_shift + $j, $il) : $subseq;
+		if ($subseqj =~ /^($dbr)/) {
+			$i_chosen = $icut < 0 ? substr($1, 0, $icut) : $1;
+			$dbrs{$i_chosen}++;
+			return $il + $i_shift + $j;
+		}
+	}
+	foreach my $rescue (@i_rescue) {
+		if ($subseq =~ /^($rescue)/) {
+			$i_chosen = $icut < 0 ? substr($1, 0, $icut) : $1;
+			$dbrs{$i_chosen}++;
+			return $il + $i_shift;
+		}
+	}
+	return -1;
+}
+
+## detect if this is a duplicate ##
+sub is_duplicate {
+	$seq = shift;
+	$subseq1 = substr($seq, 0,  $m);
+	$subseq2 = substr($seq, $m, $m);
+	$locus = $pieces12{$b_chosen}{$subseq1}{$subseq2};
+	if (!defined($locus)) {
+		$subseq3 = substr($seq, $m2, $m);
+		$locus = $pieces13{$b_chosen}{$subseq1}{$subseq3};
+		if (!defined($locus)) {
+			$locus = $pieces23{$b_chosen}{$subseq2}{$subseq3};
+			if (!defined($locus)) {
+				$locus = push(@{$loci{$b_chosen}}, $subseq1.$subseq2.$subseq3) - 1;
+				$pieces23{$b_chosen}{$subseq2}{$subseq3} = $locus;
+			}
+			$pieces13{$b_chosen}{$subseq1}{$subseq3} = $locus;
+		}
+		$pieces12{$b_chosen}{$subseq1}{$subseq2} = $locus;
+	}
+	if ($no_dbr) {
+		return (++$uniq{$b_chosen}[$locus]{"no_dbr"} > 1);
+	}
+	if ($x) {
+		foreach my $dbr (keys %{$uniq{$b_chosen}[$locus]}) {
+			$mask = $i_chosen ^ $dbr;
+			$mask =~ tr/\0//d;
+			if (length($mask) <= $x) {
+				++$uniq{$b_chosen}[$locus]{$dbr};
+				return 1;
+			}
+		}
+	}
+	return (++$uniq{$b_chosen}[$locus]{$i_chosen} > 1);
+}
+
+sub openin {
+	my $fname = shift;
+	my $fp;
+	my $pipe = ($fname =~ /\.gz$/ ? "gzip -cd $fname |" : "< $fname");
+	open($fp, $pipe) or die("$!\n");
+	return $fp;
+}
+
+sub openout {
+	my $fname = shift;
+	my $fp;
+	my $pipe = ($gz eq 'y' ? "| gzip -c > $fname.gz" : "> $fname");
+	open($fp, $pipe) or die("$!\n");
+	return $fp;
+}
+
+sub report_time {
+	$time = time() - $start_time;
+	printf STDERR "Read #%d, %d sec, %d reads/sec\n", $c_total, $time, $c_total / $time if $time > 0;
+}
